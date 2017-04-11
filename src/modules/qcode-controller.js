04489612515032662de6ce5821de2eac74827a42
sunxia628 <13939007245@163.com>
2017-04-11 17:53:01 +0800

    优化-二维码列表样式

--- src/modules/qcode-controller.js
+++ src/modules/qcode-controller.js
@@ -343,7 +343,7 @@
                     self.loading = true;
                     self.tableParams = new NgTableParams({
                         page: 1,
-                        count: 15, //15
+                        count: 10, //10
                         url: ''
                     }, {
                         counts: [],
