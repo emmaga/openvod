9b283f7e8a1b00eb9c18cd1d637bdd724d50224a
Emma <emma_0812@msn.cn>
2017-04-13 11:49:55 +0800

    新增 会员卡 配置弹窗

--- src/modules/member-card-controllers.js
+++ src/modules/member-card-controllers.js
@@ -7,7 +7,7 @@
         function ($scope, $filter, $q, $state, $http, $stateParams, NgTableParams, util) {
             var self = this;
             
-            self.init = function() {
+            self.init = function () {
                 
             }
 
@@ -18,8 +18,28 @@
         function ($scope, $filter, $q, $state, $http, $stateParams, NgTableParams, util) {
             var self = this;
             
-            self.init = function() {
+            self.init = function () {
                 
+            }
+
+            self.addLevel = function () {
+                $scope.app.maskParams = {};
+                $scope.app.showHideMask(true,'pages/memberCard/addLevel.html');
+            }
+
+            self.configUpgrade = function () {
+                $scope.app.maskParams = {};
+                $scope.app.showHideMask(true,'pages/memberCard/configUpgrade.html');
+            }
+
+            self.editMemberCard = function () {
+                $scope.app.maskParams = {};
+                $scope.app.showHideMask(true,'pages/memberCard/editMemberCard.html');
+            }
+
+            self.configMemberCard = function () {
+                $scope.app.maskParams = {};
+                $scope.app.showHideMask(true,'pages/memberCard/configMemberCard.html');
             }
 
         }
@@ -29,11 +49,119 @@
         function ($scope, $filter, $q, $state, $http, $stateParams, NgTableParams, util) {
             var self = this;
             
-            self.init = function() {
+            self.init = function () {
                 
             }
 
-        }
-    ])    
+            self.editScores = function () {
+                $scope.app.maskParams = {};
+                $scope.app.showHideMask(true,'pages/memberCard/editScores.html');
+            }
 
+            self.editLevels = function () {
+                $scope.app.maskParams = {};
+                $scope.app.showHideMask(true,'pages/memberCard/editLevels.html');
+            }
+
+            self.memberDetail = function () {
+                $scope.app.maskParams = {};
+                $scope.app.showHideMask(true,'pages/memberCard/memberDetail.html');
+            }
+        }
+    ])
+
+    .controller('editScoresController', ['$scope', '$filter', '$q', '$state', '$http', '$stateParams', 'util',
+        function ($scope, $filter, $q, $state, $http, $stateParams, util) {
+            var self = this;
+            
+            self.init = function () {
+                
+            }
+
+            self.submit = function () {
+
+            }
+
+            self.close = function () {
+                $scope.app.showHideMask(false);
+            }
+
+        }
+    ])   
+
+    .controller('editLevelsController', ['$scope', '$filter', '$q', '$state', '$http', '$stateParams', 'util',
+        function ($scope, $filter, $q, $state, $http, $stateParams, util) {
+            var self = this;
+            
+            self.init = function () {
+                
+            }
+
+            self.submit = function () {
+
+            }
+
+            self.close = function () {
+                $scope.app.showHideMask(false);
+            }
+
+        }
+    ]) 
+
+    .controller('addLevelController', ['$scope', '$filter', '$q', '$state', '$http', '$stateParams', 'util',
+        function ($scope, $filter, $q, $state, $http, $stateParams, util) {
+            var self = this;
+            
+            self.init = function () {
+                
+            }
+
+            self.submit = function () {
+
+            }
+
+            self.close = function () {
+                $scope.app.showHideMask(false);
+            }
+
+        }
+    ])
+
+    .controller('editMemberCardController', ['$scope', '$filter', '$q', '$state', '$http', '$stateParams', 'util',
+        function ($scope, $filter, $q, $state, $http, $stateParams, util) {
+            var self = this;
+            
+            self.init = function () {
+                
+            }
+
+            self.submit = function () {
+
+            }
+
+            self.close = function () {
+                $scope.app.showHideMask(false);
+            }
+
+        }
+    ]) 
+
+    .controller('configMemberCardController', ['$scope', '$filter', '$q', '$state', '$http', '$stateParams', 'util',
+        function ($scope, $filter, $q, $state, $http, $stateParams, util) {
+            var self = this;
+            
+            self.init = function () {
+                
+            }
+
+            self.submit = function () {
+
+            }
+
+            self.close = function () {
+                $scope.app.showHideMask(false);
+            }
+
+        }
+    ])         
 })();
