f858e1bcb6fdb79990217296de0476c80135dd34
sunxia628 <13939007245@163.com>
2017-04-14 10:34:03 +0800

    修改-会员管理界面-测试分页效果

--- src/modules/member-card-controllers.js
+++ src/modules/member-card-controllers.js
@@ -2,166 +2,214 @@
 
 (function () {
     var app = angular.module('app.member-card-controllers', [])
+        //会员信息管理列表
+        .controller('memberListController', ['$scope', '$filter', '$q', '$state', '$http', '$stateParams', 'NgTableParams', 'util',
+            function ($scope, $filter, $q, $state, $http, $stateParams, NgTableParams, util) {
+                var self = this;
 
-    .controller('memberCardController', ['$scope', '$filter', '$q', '$state', '$http', '$stateParams', 'NgTableParams', 'util',
-        function ($scope, $filter, $q, $state, $http, $stateParams, NgTableParams, util) {
-            var self = this;
-            
-            self.init = function () {
-                
+                self.init = function () {
+                    self.langStyle = util.langStyle();
+                    self.multiLang = util.getParams('editLangs');
+                    self.getMemberInfo();
+                }
+                // 获取会员用户信息列表
+                self.getMemberInfo = function() {
+                    self.noData = false;
+                    self.loading = true;
+                    self.tableParams = new NgTableParams({
+                        page: 1,
+                        count: 15,
+                        url: ''
+                    }, {
+                        counts: [],
+                        getData: function(params) {
+                            var data = {
+                                "action": "getWxUserInfo",
+                                "token": util.getParams("token"),
+                                "lang": self.langStyle
+                            }
+                            var paramsUrl = params.url();
+                            data.per_page = paramsUrl.count - 0;
+                            data.page = paramsUrl.page - 0;
+                            data = JSON.stringify(data);
+                            return $http({
+                                method: $filter('ajaxMethod')(),
+                                url: util.getApiUrl('devinfo', 'shopList', 'server'),
+                                data: data
+                            }).then(function successCallback(data, status, headers, config) {
+                                if (data.data.rescode == '200') {
+                                    if (data.data.total == 0) {
+                                        self.noData = true;
+                                    }
+                                    params.total(data.data.total);
+                                    return data.data.userinfo;
+                                } else if (msg.rescode == '401') {
+                                    alert('访问超时，请重新登录');
+                                    $location.path("pages/login.html");
+                                } else {
+                                    alert(data.rescode + ' ' + data.errInfo);
+                                }
+
+                            }, function errorCallback(data, status, headers, config) {
+                                alert(response.status + ' 服务器出错');
+                            }).finally(function(value) {
+                                self.loading = false;
+                            })
+                        }
+                    });
+                }
+                self.editScores = function () {
+                    $scope.app.maskParams = {};
+                    $scope.app.showHideMask(true, 'pages/memberCard/editScores.html');
+                }
+
+                self.editLevels = function () {
+                    $scope.app.maskParams = {};
+                    $scope.app.showHideMask(true, 'pages/memberCard/editLevels.html');
+                }
+
+                self.memberDetail = function () {
+                    $scope.app.maskParams = {};
+                    $scope.app.showHideMask(true, 'pages/memberCard/memberDetail.html');
+                }
             }
+        ])
+        //会员管理
+        .controller('memberCardController', ['$scope', '$filter', '$q', '$state', '$http', '$stateParams', 'NgTableParams', 'util',
+            function ($scope, $filter, $q, $state, $http, $stateParams, NgTableParams, util) {
+                var self = this;
 
-        }
-    ])
+                self.init = function () {
 
-    .controller('memberCardListController', ['$scope', '$filter', '$q', '$state', '$http', '$stateParams', 'NgTableParams', 'util',
-        function ($scope, $filter, $q, $state, $http, $stateParams, NgTableParams, util) {
-            var self = this;
-            
-            self.init = function () {
-                
-            }
-
-            self.addLevel = function () {
-                $scope.app.maskParams = {};
-                $scope.app.showHideMask(true,'pages/memberCard/addLevel.html');
-            }
-
-            self.configUpgrade = function () {
-                $scope.app.maskParams = {};
-                $scope.app.showHideMask(true,'pages/memberCard/configUpgrade.html');
-            }
-
-            self.editMemberCard = function () {
-                $scope.app.maskParams = {};
-                $scope.app.showHideMask(true,'pages/memberCard/editMemberCard.html');
-            }
-
-            self.configMemberCard = function () {
-                $scope.app.maskParams = {};
-                $scope.app.showHideMask(true,'pages/memberCard/configMemberCard.html');
-            }
-
-        }
-    ])
-
-    .controller('memberListController', ['$scope', '$filter', '$q', '$state', '$http', '$stateParams', 'NgTableParams', 'util',
-        function ($scope, $filter, $q, $state, $http, $stateParams, NgTableParams, util) {
-            var self = this;
-            
-            self.init = function () {
-                
-            }
-
-            self.editScores = function () {
-                $scope.app.maskParams = {};
-                $scope.app.showHideMask(true,'pages/memberCard/editScores.html');
-            }
-
-            self.editLevels = function () {
-                $scope.app.maskParams = {};
-                $scope.app.showHideMask(true,'pages/memberCard/editLevels.html');
-            }
-
-            self.memberDetail = function () {
-                $scope.app.maskParams = {};
-                $scope.app.showHideMask(true,'pages/memberCard/memberDetail.html');
-            }
-        }
-    ])
-
-    .controller('editScoresController', ['$scope', '$filter', '$q', '$state', '$http', '$stateParams', 'util',
-        function ($scope, $filter, $q, $state, $http, $stateParams, util) {
-            var self = this;
-            
-            self.init = function () {
-                
-            }
-
-            self.submit = function () {
-
-            }
-
-            self.close = function () {
-                $scope.app.showHideMask(false);
-            }
-
-        }
-    ])   
-
-    .controller('editLevelsController', ['$scope', '$filter', '$q', '$state', '$http', '$stateParams', 'util',
-        function ($scope, $filter, $q, $state, $http, $stateParams, util) {
-            var self = this;
-            
-            self.init = function () {
-                
-            }
-
-            self.submit = function () {
+                }
 
             }
+        ])
+        //会员卡
+        .controller('memberCardListController', ['$scope', '$filter', '$q', '$state', '$http', '$stateParams', 'NgTableParams', 'util',
+            function ($scope, $filter, $q, $state, $http, $stateParams, NgTableParams, util) {
+                var self = this;
 
-            self.close = function () {
-                $scope.app.showHideMask(false);
-            }
+                self.init = function () {
 
-        }
-    ]) 
+                }
 
-    .controller('addLevelController', ['$scope', '$filter', '$q', '$state', '$http', '$stateParams', 'util',
-        function ($scope, $filter, $q, $state, $http, $stateParams, util) {
-            var self = this;
-            
-            self.init = function () {
-                
-            }
+                self.addLevel = function () {
+                    $scope.app.maskParams = {};
+                    $scope.app.showHideMask(true, 'pages/memberCard/addLevel.html');
+                }
 
-            self.submit = function () {
+                self.configUpgrade = function () {
+                    $scope.app.maskParams = {};
+                    $scope.app.showHideMask(true, 'pages/memberCard/configUpgrade.html');
+                }
 
-            }
+                self.editMemberCard = function () {
+                    $scope.app.maskParams = {};
+                    $scope.app.showHideMask(true, 'pages/memberCard/editMemberCard.html');
+                }
 
-            self.close = function () {
-                $scope.app.showHideMask(false);
-            }
-
-        }
-    ])
-
-    .controller('editMemberCardController', ['$scope', '$filter', '$q', '$state', '$http', '$stateParams', 'util',
-        function ($scope, $filter, $q, $state, $http, $stateParams, util) {
-            var self = this;
-            
-            self.init = function () {
-                
-            }
-
-            self.submit = function () {
+                self.configMemberCard = function () {
+                    $scope.app.maskParams = {};
+                    $scope.app.showHideMask(true, 'pages/memberCard/configMemberCard.html');
+                }
 
             }
+        ])
 
-            self.close = function () {
-                $scope.app.showHideMask(false);
+        //调分
+        .controller('editScoresController', ['$scope', '$filter', '$q', '$state', '$http', '$stateParams', 'util',
+            function ($scope, $filter, $q, $state, $http, $stateParams, util) {
+                var self = this;
+
+                self.init = function () {
+
+                }
+
+                self.submit = function () {
+
+                }
+
+                self.close = function () {
+                    $scope.app.showHideMask(false);
+                }
+
             }
+        ])
+        //调级
+        .controller('editLevelsController', ['$scope', '$filter', '$q', '$state', '$http', '$stateParams', 'util',
+            function ($scope, $filter, $q, $state, $http, $stateParams, util) {
+                var self = this;
 
-        }
-    ]) 
+                self.init = function () {
 
-    .controller('configMemberCardController', ['$scope', '$filter', '$q', '$state', '$http', '$stateParams', 'util',
-        function ($scope, $filter, $q, $state, $http, $stateParams, util) {
-            var self = this;
-            
-            self.init = function () {
-                
+                }
+
+                self.submit = function () {
+
+                }
+
+                self.close = function () {
+                    $scope.app.showHideMask(false);
+                }
+
             }
+        ])
+        //新建会员卡等级
+        .controller('addLevelController', ['$scope', '$filter', '$q', '$state', '$http', '$stateParams', 'util',
+            function ($scope, $filter, $q, $state, $http, $stateParams, util) {
+                var self = this;
 
-            self.submit = function () {
+                self.init = function () {
+
+                }
+
+                self.submit = function () {
+
+                }
+
+                self.close = function () {
+                    $scope.app.showHideMask(false);
+                }
 
             }
+        ])
+        //会员卡配置详情
+        .controller('editMemberCardController', ['$scope', '$filter', '$q', '$state', '$http', '$stateParams', 'util',
+            function ($scope, $filter, $q, $state, $http, $stateParams, util) {
+                var self = this;
 
-            self.close = function () {
-                $scope.app.showHideMask(false);
+                self.init = function () {
+
+                }
+
+                self.submit = function () {
+
+                }
+
+                self.close = function () {
+                    $scope.app.showHideMask(false);
+                }
+
             }
+        ])
+        //微信-会员卡-等级-地址配置
+        .controller('configMemberCardController', ['$scope', '$filter', '$q', '$state', '$http', '$stateParams', 'util',
+            function ($scope, $filter, $q, $state, $http, $stateParams, util) {
+                var self = this;
 
-        }
-    ])         
+                self.init = function () {
+
+                }
+
+                self.submit = function () {
+
+                }
+
+                self.close = function () {
+                    $scope.app.showHideMask(false);
+                }
+
+            }
+        ])
 })();
