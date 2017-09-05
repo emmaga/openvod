87a22af278d134dd241f37b35a35cd7f3d219064
raythunder <raythunder@163.com>
2017-09-05 15:27:36 +0800

    新增 商品列表、发票状态分类。增加已开票按钮

--- src/modules/orders-controllers.js
+++ src/modules/orders-controllers.js
@@ -372,6 +372,12 @@
                 self.loadShopList().then(function () {
                     self.search();
                 });
+
+                $scope.billStatus=''
+
+                $scope.$watch('billStatus', function () {
+                    self.search()
+                }, true);
             }
 
             self.getSelectedShop = function () {
@@ -523,6 +529,37 @@
                 });
             }
 
+            self.setInvoice = function (id) {
+                var data = JSON.stringify({
+                    "token": util.getParams('token'),
+                    "action": "setInvoiceStatus",
+                    "lang": util.langStyle(),
+                    "OrderID": id,
+                    "Status":2
+                })
+
+                $http({
+                    method: 'POST',
+                    url: util.getApiUrl('shoporder', '', 'server'),
+                    data: data
+                }).then(function successCallback(response) {
+                    var data = response.data;
+                    if (data.rescode == '200') {
+                        alert('操作已成功');
+                        self.search();
+                    } else if (data.rescode == '401') {
+                        alert('访问超时，请重新登录');
+                        $state.go('login');
+                    } else {
+                        alert('操作失败' + data.errInfo);
+                    }
+                }, function errorCallback(response) {
+                    alert('连接服务器出错');
+                }).finally(function (value) {
+
+                });
+            }
+
             self.searchByStatus = function (statusCode) {
                 for (var i = 0; i < $scope.status.length; i++) {
                     if ($scope.status[i].code == statusCode) {
@@ -616,7 +653,8 @@
                                 "ContactorName": self.searchStr.userName,
                                 "OrderNum": self.searchStr.orderNumber,
                                 "page": paramsUrl.page - 0,
-                                "per_page": paramsUrl.count - 0
+                                "per_page": paramsUrl.count - 0,
+                                "InvoiceStatus": $scope.billStatus
                             });
                             self.loading = true;
                             self.noData = false;
